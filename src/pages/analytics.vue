19cef57f3da64c285769470914d2a2c24abfda42
Danny Fung <dannyfung97@gmail.com>
2022-12-07 22:12:17 -0800

    feat: analytics formatting

--- src/pages/analytics.vue
+++ src/pages/analytics.vue
@@ -3,8 +3,23 @@ import axios from 'axios';
 import { onBeforeMount, ref } from 'vue';
 
 import { isMumbai } from '@/composables/useNetwork';
+import useNumbers from '@/composables/useNumbers';
+const { fNum, fNum2 } = useNumbers();
 
 const data = ref<any[]>(Array(13).fill(0));
+
+function convertSeconds(seconds: number) {
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+
+  const dDisplay = d > 0 ? d + 'd ' : '';
+  const hDisplay = h > 0 ? h + 'h ' : '';
+  const mDisplay = m > 0 ? m + 'm ' : '';
+  const sDisplay = s > 0 ? s + 's ' : '';
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+}
 
 async function init() {
   const url = `https://api.holdr.fi/analytics${isMumbai ? '-mumbai' : ''}`;
@@ -40,7 +55,7 @@ onBeforeMount(() => {
           24H Volume
         </div>
         <div class="value text-center">
-          <span class="truncate text-5xl">{{ data[6] }}</span>
+          <span class="text-5xl">{{ fNum(data[6], 'usd_lg') }}</span>
         </div>
       </BalCard>
       <BalCard square class="p-8">
@@ -48,7 +63,7 @@ onBeforeMount(() => {
           7D Volume
         </div>
         <div class="value text-center">
-          <span class="truncate text-5xl">{{ data[7] }}</span>
+          <span class="text-5xl">{{ fNum(data[7], 'usd_lg') }}</span>
         </div>
       </BalCard>
     </div>
@@ -59,7 +74,7 @@ onBeforeMount(() => {
           Total veHLDR
         </div>
         <div class="value text-center">
-          <span class="truncate text-lg">{{ data[8] }}</span>
+          <span class="text-lg">{{ fNum(data[8], 'token') }}</span>
         </div>
       </BalCard>
       <BalCard square>
@@ -67,7 +82,7 @@ onBeforeMount(() => {
           80HLDR-20WNEAR Locked
         </div>
         <div class="value text-center">
-          <span class="truncate text-lg">{{ data[10] }}</span>
+          <span class="text-lg">{{ fNum(data[10], 'token') }}</span>
         </div>
       </BalCard>
       <BalCard square>
@@ -75,7 +90,7 @@ onBeforeMount(() => {
           80HLDR-20WNEAR
         </div>
         <div class="value text-center">
-          <span class="truncate text-lg">{{ data[9] }}</span>
+          <span class="text-lg">{{ fNum(data[9], 'token') }}</span>
         </div>
       </BalCard>
       <BalCard square>
@@ -83,7 +98,7 @@ onBeforeMount(() => {
           Percentage Locked
         </div>
         <div class="value text-center">
-          <span class="truncate text-lg">{{ data[12] }}</span>
+          <span class="text-lg">{{ fNum(data[12], 'percent') }}</span>
         </div>
       </BalCard>
       <BalCard square>
@@ -91,7 +106,7 @@ onBeforeMount(() => {
           veHLDR Average Lock Time
         </div>
         <div class="value text-center">
-          <span class="truncate text-lg">{{ data[11] }}</span>
+          <span class="text-lg">{{ convertSeconds(data[11]) }}</span>
         </div>
       </BalCard>
     </div>
@@ -102,7 +117,7 @@ onBeforeMount(() => {
           Total Value Locked
         </div>
         <div class="value text-center">
-          <span class="truncate text-3xl">{{ data[2] }}</span>
+          <span class="text-3xl">{{ fNum(data[2], 'usd') }}</span>
         </div>
       </BalCard>
       <BalCard square class="p-5">
@@ -110,7 +125,7 @@ onBeforeMount(() => {
           # Pools
         </div>
         <div class="value text-center">
-          <span class="truncate text-3xl">{{ data[0] }}</span>
+          <span class="text-3xl">{{ fNum(data[0], 'default') }}</span>
         </div>
       </BalCard>
       <BalCard square class="p-5">
@@ -118,7 +133,7 @@ onBeforeMount(() => {
           LPs
         </div>
         <div class="value text-center">
-          <span class="truncate text-3xl">{{ data[1] }}</span>
+          <span class="text-3xl">{{ fNum(data[1], 'default') }}</span>
         </div>
       </BalCard>
     </div>
@@ -129,7 +144,7 @@ onBeforeMount(() => {
           HLDR Minted
         </div>
         <div class="value text-center">
-          <span class="truncate text-3xl"> {{ data[4] }} </span>
+          <span class="text-3xl"> {{ fNum(data[4], 'token') }} </span>
         </div>
       </BalCard>
       <BalCard square class="p-5">
@@ -137,7 +152,7 @@ onBeforeMount(() => {
           # HLDR Holders
         </div>
         <div class="value text-center">
-          <span class="truncate text-3xl">{{ data[3] }}</span>
+          <span class="text-3xl">{{ fNum(data[3], 'default') }}</span>
         </div>
       </BalCard>
       <BalCard square class="p-5">
@@ -145,7 +160,7 @@ onBeforeMount(() => {
           # HLDR Holders and LPs
         </div>
         <div class="value text-center">
-          <span class="truncate text-3xl">{{ data[5] }}</span>
+          <span class="text-3xl">{{ fNum(data[5], 'default') }}</span>
         </div>
       </BalCard>
     </div>
