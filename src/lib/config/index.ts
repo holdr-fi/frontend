b3e4d1d5c58969cf7059b20685be20d2a233c2cf
Danny Fung <dannyfung97@gmail.com>
2022-12-23 15:29:52 -0800

    feat: added batchRelayerV4 address field to network config

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -58,6 +58,7 @@ export interface Config {
     lidoRelayer: string;
     balancerHelpers: string;
     batchRelayer: string;
+    batchRelayerV4: string;
     veBAL: string;
     gaugeFactory: string;
     balancerMinter: string;
